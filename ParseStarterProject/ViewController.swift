e3c243f2277c7319485b623b40b4ef50807c8f3a
Bradford Gray <bgray@westmont.edu>
2015-08-24 22:07:16 -0700

    login

---
+++ ParseStarterProject/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//
+//  Copyright 2011-present Parse Inc. All rights reserved.
+//
+
+import UIKit
+import Parse
+import FBSDKCoreKit
+
+class ViewController: UIViewController {
+
+    @IBAction func SignIn(sender: UIButton) {
+   
+        
+        let permissions = ["public_profile"]
+        
+        PFFacebookUtils.logInInBackgroundWithReadPermissions(permissions, block: {
+            
+            (user: PFUser?, error: NSError?) -> Void in
+            
+            if let error = error {
+                print(error)
+            } else {
+                if let user = user {
+                    self.performSegueWithIdentifier("showSigninScreen", sender: self)
+                }
+            }
+            
+        })
+        
+        
+
+    
+    
+    
+    }
+    
+
+    override func viewDidAppear(animated: Bool) {
+        
+               if let user = PFUser.currentUser()?.username {
+        
+        performSegueWithIdentifier("showSigninScreen", sender: self)
+        }
+    }
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
